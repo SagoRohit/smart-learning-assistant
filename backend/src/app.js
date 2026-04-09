a94960721341f710d5e37677f0290b515ac15547
Sagor Chanda <ch.sagor27@gmail.com>
2026-04-10 00:13:58 +0600

    connect routes in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const subjectRoutes = require("./routes/subjectRoutes");
+const caseRoutes = require("./routes/caseRoutes");
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +12,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/subjects", subjectRoutes);
+app.use("/api/cases", caseRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
